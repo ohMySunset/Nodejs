6d9503217df6dbb34188f960be22654d96944ffd
ohMySunset <znfnznfnakqj@naver.com>
2021-05-23 23:47:20 +0900

    Update : Day4 - 루트경로 요청응답하기 - 20210523

--- web1_html_internet/main.js
+++ web1_html_internet/main.js
@@ -5,46 +5,74 @@ var url = require('url');
 var app = http.createServer(function(request, response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    console.log(queryData.id);
+    var pathname = url.parse(_url, true).pathname;
     console.log(_url);
     var title = queryData.id;
 
-    if(_url == '/'){
-        title = 'Welcome';
-    }
-
-    if(_url == '/favicon.ico'){
-        return response.writeHead(404);
-    }
-
-    response.writeHead(200);
+    console.log(url.parse(_url, true));
     console.log(__dirname + _url);
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
 
-      var template = `
-      <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    <ul>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=JavaScript">JavaScript</a></li>
-    </ul>
-    <h2>${title}</h2>
-    <p>${description}</p>
-  </body>
-  </html>
-  `;
-      response.end(template);
-      //response.end('egoing : '+ url);
+    if(pathname === '/'){
+      // 루트라면 기존 코드를 실행
+      if(queryData.id === undefined){
+      // 홈 요청일 때 (= 쿼리스트링이 없을 때)
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+        var template = `
+        <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ul>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ul>
+      <h2>${title}</h2>
+      <p>${description}</p>
+    </body>
+    </html>
+    `;   
+        response.writeHead(200);
+        response.end(template);
+   
+    } else {
+      // 홈 요청이 아닐때
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var title = queryData.id;
+        var template = `
+        <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ul>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ul>
+      <h2>${title}</h2>
+      <p>${description}</p>
+    </body>
+    </html>
+    `;   
+        response.writeHead(200);
+        response.end(template);
+      });
+    } 
 
-    });
-
+    } else {
+      // 루트가 아니라면 새로운 코드를 실행
+      response.writeHead(404);
+      response.end('Not Found');
+    }
 });
 
 app.listen(3000);
