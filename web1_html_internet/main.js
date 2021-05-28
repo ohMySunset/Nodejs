850deeed5c41e0ff778f7e46d215ca334c4681f6
ohMySunset <znfnznfnakqj@naver.com>
2021-05-29 00:35:11 +0900

    Update : Day7 - Post방식으로 전송데이터 받기 - 20210529

--- web1_html_internet/main.js
+++ web1_html_internet/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body){
   return `
@@ -13,6 +14,7 @@ function templateHTML(title, list, body){
     <body>
       <h1><a href="/">WEB1</a></h1>
       ${list}
+      <a href="/create">create</a> 
       ${body}
     </body>
     </html>
@@ -75,7 +77,46 @@ var app = http.createServer(function(request, response){
       });
     });
     } 
-    } else {
+    } else if(pathname === '/create'){
+      // 파일목록 불러오기
+      fs.readdir('../web1_html_internet/data', function(error, filelist){
+
+      // 홈 요청일 때 (= 쿼리스트링이 없을 때)
+      var title = 'WEB - Create';
+      var list = templateList(filelist);
+      var template = templateHTML(title, list , `
+        <form action="http://localhost:3000/create_process" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+              <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+              <input type="submit"> 
+          </p>
+        </form>
+      `);
+     
+      response.writeHead(200);
+      response.end(template);
+      });
+    } else if(pathname === '/create_process'){
+      var body = '';
+      // 데이터를 수신할 때 마다 호출되는 콜백함수
+      request.on('data', function(data){
+        body = body + data;
+      });
+      // 더이상 수신할 데이터가 없는 경우 호출되는 콜백함수
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        console.log(title);
+        console.log(description);
+      });
+      response.writeHead(200);
+      response.end('success');
+    }  
+    else {
       // 루트가 아니라면 새로운 코드를 실행
       response.writeHead(404);
       response.end('Not Found');
