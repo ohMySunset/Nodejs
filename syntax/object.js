45fccf9aebbd0c88189f4a978199778041b31034
ohMySunset <znfnznfnakqj@naver.com>
2021-06-02 00:06:54 +0900

    Update : Day9 - 자바스크립트 객체 - 20210602

---
+++ syntax/object.js
@@ -0,0 +1,12 @@
+// 배열의 생성과 사용
+var members = ['egoing', 'k8805', 'hoya'];
+//onsole.log(members[1]);
+
+// 객체의 생성과 사용
+var roles={
+    'programmer' : 'egoing',
+    'designer' : 'k8805',
+    'manager' : 'hoya'
+}
+console.log(roles.designer);
+console.log(roles['designer']);
