d43f7ddf03177676cce4a0e4db47caf8b05d3efb
ohMySunset <znfnznfnakqj@naver.com>
2021-06-28 00:02:43 +0900

    Update : Day16 - Express 사용준비 - 20210628

--- 03_express/main.js
+++ 03_express/main.js
@@ -151,8 +151,13 @@ const express = require('express');
 const app = express();
 
 // app.get(path, callback[ , callback, ..])
-app.get('/', (req, res) => res.send('Hello World!'))
+//app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/', function(req, res){
-    return res.send('Hello World!')
+    return res.send('/')
 });
-app.listen(3000, ()=> console.log('Example app listening on port 3000!'))
+app.get('/page', function(req, res){
+    return res.send('/page')
+});
+app.listen(3000, function(){
+    console.log('Example app listening on port 3000!')
+}); 
